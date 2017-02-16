59fa6b705dfd6c95b7ef2e1ecc134b519197e116
陈浩 <chenhao@wanfin.com>
2017-02-16 22:03:53 +0800

    init spring-data-jpa

---
+++ src/main/java/com/example/entity/Player.java
@@ -0,0 +1,29 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+public class Player {
+
+	@Id 
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Integer age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+}
---
+++ src/main/java/com/example/entity/Team.java
@@ -0,0 +1,43 @@
+package com.example.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+public class Team {
+	@Id 
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Integer age;
+	
+	@JoinColumn(name="teamId")
+	@OneToMany(cascade=CascadeType.ALL)
+	private List<Player> players;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public List<Player> getPlayers() {
+		return players;
+	}
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+}
